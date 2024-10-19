e5036d2681ea065b404f39bc7bd6d4403b1c5024
Kirit-Jain <kirit.ajain@gmail.com>
2024-10-19 14:51:44 +0530

    weirdAlgorithm solution

---
+++ weirdAlgorithm.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    long n;
+    
+    cin>>n;
+    if(n>=1 && n<=1000000)
+    {
+        cout<<n<<" ";
+        while(n != 1)
+        {
+            if(n%2 != 0)
+            {
+                n = n*3 + 1;
+                cout<<n<<" ";
+            }
+            else
+            {
+                n=n/2;
+                cout<<n<<" ";
+            }
+        }
+    }
+}
