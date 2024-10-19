e759c15d9fe8c16f8bdb706f2b06c4d4f88ad2e7
Kirit-Jain <kirit.ajain@gmail.com>
2024-10-19 15:24:53 +0530

    missingNumber solution

--- IntroductoryProblems/missingNumber.cpp
+++ IntroductoryProblems/missingNumber.cpp
@@ -1,7 +1,29 @@
-#include<iostream>
+//Problem Statement https://cses.fi/problemset/task/1083/
+
+#include<bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    int n
+    long n;
+    cin>>n;
+    vector<long> v;
+    for(long i = 0; i<n-1 ;i++)
+    {
+        long a;
+        cin>>a;
+        v.push_back(a);
+    }
+
+    long total;
+    long sum = 0;
+    total = (n*(n+1))/2;
+    for(long i = 0; i < n-1; i++)
+    {
+        sum = sum + v[i];
+    }
+
+    long out = total - sum;
+
+    cout<<out;
 }
